1279a3eec4edb2c11ceeb3c0afcd36cd53beca6d
Nick Atkinson <nick@daileyproject.com>
2019-06-20 10:48:06 -0700

    Added Users resource and adjusted code for readability

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from resources.store import Store, StoreList
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'hGrk43fasSgRens96fa39fk30'
 api = Api(app)
 
@@ -20,9 +19,10 @@ jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
-api.add_resource(UserRegister, '/register')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
+api.add_resource(UserList, '/users')
+api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
 	from db import db
