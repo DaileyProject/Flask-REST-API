7fc04932a2c247b91c1641e1aa404ecacd4b5311
Nick Atkinson <nick@daileyproject.com>
2019-06-20 14:10:13 -0700

    Adding JWT Extended - Error fixes

--- resources/user.py
+++ resources/user.py
@@ -50,7 +50,7 @@ class UserAuth(Resource):
 
 		if user and safe_str_cmp(user.password, data['password']):
 			access_token = create_access_token(identity=user.id, fresh=True)
-			refresh_toek = create_refresh_token(user.id)
+			refresh_token = create_refresh_token(user.id)
 			return {
 				'access_token': access_token,
 				'refresh_token': refresh_token
