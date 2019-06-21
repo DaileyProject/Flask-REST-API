4cad80d537909ad52cb2966c6bfba5952939c8cb
Nick Atkinson <nick@daileyproject.com>
2019-06-21 10:06:55 -0700

    Adding JWT Extended - Token refresh

--- resources/user.py
+++ resources/user.py
@@ -69,6 +69,7 @@ class UserAuth(Resource):
 
 class TokenRefresh(Resource):
 	@jwt_refresh_token_required
-	current_user = get_jwt_identity()
-	new_token = create_access_token(identity=user, fresh=False)
-	return {'access_token': new_token}, 200
+	def post(self):
+		current_user = get_jwt_identity()
+		new_token = create_access_token(identity=user, fresh=False)
+		return {'access_token': new_token}, 200
