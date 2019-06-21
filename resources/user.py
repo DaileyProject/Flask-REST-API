090c4cfc4e781d75f5bc2dd52741b790dd458e52
Nick Atkinson <nick@daileyproject.com>
2019-06-21 10:04:42 -0700

    Adding JWT Extended - Token refresh

--- resources/user.py
+++ resources/user.py
@@ -4,7 +4,7 @@ from flask_jwt_extended import (
 	create_access_token,
 	create_refresh_token,
 	jwt_required,
-	jwt_refresh_token,
+	jwt_refresh_token_required,
 	get_jwt_identity
 )
 from models.user import UserModel
